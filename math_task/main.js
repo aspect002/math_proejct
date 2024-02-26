9f38009b7d49a4b27e956be15ddbb3cccccc763c
Stas <stas.krivosheev.0054@gmail.com>
2024-02-26 14:50:43 +0300

    add first commit

---
+++ math_task/main.js
@@ -0,0 +1,7 @@
+const mathFunctions = require('./math');
+
+
+console.log(mathFunctions.addition(2,3));
+console.log(mathFunctions.subtraction(2,3));
+console.log(mathFunctions.multiplication(2,3));
+console.log(mathFunctions.division(2,4));
---
+++ math_task/math.js
@@ -0,0 +1,14 @@
+const addition = (a,b) => a + b;
+
+const subtraction = (a,b) => a - b;
+
+const multiplication = (a,b) => a * b;
+
+const division = (a,b) => a / b;
+
+module.exports = {
+    addition,
+    subtraction,
+    multiplication,
+    division
+};
